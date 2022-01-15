5ca0a85de7b9f0b7b76558cb81cf32c9e4cfb57e
Drako <abhishek491981@gmail.com>
2022-01-15 18:42:17 +0530

    appBar added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,49 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, Key? key}) : super(key: key);
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60, //in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[900]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        // <Widget> is the type of items in the list.
+        children: [
+          const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child
+          // to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          const IconButton(
+            icon: Icon(Icons.logout),
+            tooltip: 'Extra',
+            onPressed: null,
+          ),
+          const IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,70 +54,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blue.shade100,
         body: Container(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage('images/profile-img.jpeg'),
-              ),
-              Text(
-                'Drako Rexon',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            children: [
+              MyAppBar(
+                title: Text(
+                  'Introduction APP',
+                  style: Theme.of(context) //
+                      .primaryTextTheme
+                      .headline6,
                 ),
               ),
-              Text(
-                'C++ Coder',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey.shade200,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 60,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.lightBlueAccent,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage('images/profile-img.jpeg'),
+                    
                   ),
-                  title: Text(
-                    '+91 93X XXXX X84',
+                  const Text(
+                    'Drako Rexon',
                     style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 60,
-                  horizontal: 60,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.indigo.shade700,
-                  ),
-                  title: Text(
-                    'drakocodeforever@gmail.com',
+                  Text(
+                    'C++ Coder',
                     style: TextStyle(
-                      color: Colors.black,
                       fontSize: 20,
+                      color: Colors.grey.shade200,
                     ),
                   ),
-                ),
-              )
+                  const Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 60,
+                    ),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.indigo,
+                      ),
+                      title: Text(
+                        '+91 93X XXXX X84',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.white,
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 60,
+                      horizontal: 60,
+                    ),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail,
+                        color: Colors.indigo.shade700,
+                      ),
+                      title: const Text(
+                        'drakocodeforever@gmail.com',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
