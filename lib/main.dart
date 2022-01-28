59e5d34b5bfc9a042a75fc008c1385271263bdbc
Drako <abhishek491981@gmail.com>
2022-01-28 17:54:12 +0530

    break1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drako\'s piano tiles',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Piano(),
+    );
+  }
+}
+
+class Piano extends StatefulWidget {
+  Piano({Key? key}) : super(key: key);
+
+  @override
+  State<Piano> createState() => _PianoState();
+}
+
+class _PianoState extends State<Piano> {
+
+  AudioCache player = new AudioCache();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
