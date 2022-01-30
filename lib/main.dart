dd5b5e122b98e737050005ce9f46b4f10f822cde
Drako <abhishek491981@gmail.com>
2022-01-30 20:42:02 +0530

    almost basics ready

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Intro App',
       home: Scaffold(
         backgroundColor: Colors.blue.shade100,
