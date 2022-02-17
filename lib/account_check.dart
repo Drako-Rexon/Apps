1e6515f8d8b1f97ec74549bc8ed8718785b772dc
Drako <abhishek491981@gmail.com>
2022-02-17 14:27:17 +0530

    2 textfield has added

--- lib/account_check.dart
+++ lib/account_check.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:yoboresto/main.dart';
 import 'package:yoboresto/signinpage.dart';
+import 'main.dart';
 
 class SignInPage extends StatefulWidget {
   SignInPage({Key? key}) : super(key: key);
@@ -15,129 +16,140 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     var devWidth = MediaQuery.of(context).size.width;
 
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(
-                top: 20,
-                left: devWidth - 200,
-              ),
-              child: Text(
-                'Explore  the app',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Color(0xFFE46829),
-                  fontSize: 15,
+    return Scaffold(
+      body: Container(
+        color: customWhite,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 20,
+                  left: devWidth - 220,
+                ),
+                child: Text(
+                  'Explore  the app',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Color(0xFFE46829),
+                    fontSize: 15,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 2,
+              SizedBox(
+                height: 40,
               ),
-              child: Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  // boxShadow:
-                  // image: DecorationImage(
-                  // image: AssetImage(/*'assets/images/'*/),
-                  // fit: BoxFit.cover,
-                  // ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 60,
+                ),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    // boxShadow:
+                    // image: DecorationImage(
+                    // image: AssetImage(/*'assets/images/'*/),
+                    // fit: BoxFit.cover,
+                    // ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 200,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 40,
-                right: 40,
-                bottom: 30,
+              SizedBox(
+                height: 100,
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Create account',
-                      style: TextStyle(
-                        color: deepOrange,
-                        fontSize: 14,
-                      ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40,
+                  right: 40,
+                  bottom: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 100,
+        color: customWhite,
+        
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40,),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Create account',
+                  style: TextStyle(
+                    color: deepOrange,
+                    fontSize: 14,
+                  ),
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 14,
                     ),
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(
-                          horizontal: 32,
-                          vertical: 14,
-                        ),
-                      ),
-                      elevation: MaterialStateProperty.all(0),
-                      backgroundColor: MaterialStateProperty.all(customWhite),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(28),
-                          side: BorderSide(
-                            width: 1.5,
-                            color: deepOrange,
-                            style: BorderStyle.solid,
-                          ),
-                        ),
+                  ),
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: MaterialStateProperty.all(customWhite),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(28),
+                      side: BorderSide(
+                        width: 1,
+                        color: deepOrange,
+                        style: BorderStyle.solid,
                       ),
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        PageTransition(
-                          type: PageTransitionType.fade,
-                          child: SignIn(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      'working',
-                      style: TextStyle(
-                        color: deepOrange,
-                        fontSize: 14,
-                      ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.fade,
+                      child: SignIn(),
                     ),
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(
-                          horizontal: 47,
-                          vertical: 14,
-                        ),
-                      ),
-                      elevation: MaterialStateProperty.all(0),
-                      backgroundColor: MaterialStateProperty.all(customWhite),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(28),
-                          side: BorderSide(
-                            width: 1.5,
-                            color: deepOrange,
-                            style: BorderStyle.solid,
-                          ),
-                        ),
+                  );
+                },
+                child: Text(
+                  'working',
+                  style: TextStyle(
+                    color: deepOrange,
+                    fontSize: 14,
+                  ),
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(
+                      horizontal: 47,
+                      vertical: 14,
+                    ),
+                  ),
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: MaterialStateProperty.all(customWhite),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(28),
+                      side: BorderSide(
+                        width: 1,
+                        color: deepOrange,
+                        style: BorderStyle.solid,
                       ),
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/signinpage.dart
+++ lib/signinpage.dart
@@ -9,116 +9,253 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  String dropdownValue = '+91';
+  var items = ['+91', '+1', '+251'];
+
   @override
   Widget build(BuildContext context) {
+    var devWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color.fromARGB(255, 255, 224, 234),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(
-                  Icons.arrow_back_ios_new,
-                  color: Color.fromARGB(255, 195, 90, 90),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 30,
+          ),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromARGB(255, 255, 224, 234),
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios_new,
+                    color: Color.fromARGB(255, 195, 90, 90),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Sign in',
-              style: TextStyle(
-                fontSize: 35,
+              SizedBox(
+                height: 20,
               ),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Using'),
-                    Text('Mobile number'),
-                  ],
+              Text(
+                'Sign in',
+                style: TextStyle(
+                  fontSize: 35,
                 ),
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 17,
-                    ),
-                    Text('Email id'),
-                  ],
-                ),
-              ],
-            ),
-            Row(), // This is for the input text area in wich the credential is inputting
-            SizedBox(height: 360,),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 30,
-                vertical: 40,
               ),
-              child: Row(
+              SizedBox(
+                height: 20,
+              ),
+              Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('New user ?'),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all(EdgeInsets.zero),
-                          elevation: MaterialStateProperty.all(0),
-                          backgroundColor:
-                              MaterialStateProperty.all(customWhite),
-                        ),
-                        child: Text(
-                          'Create an account',
-                          style: TextStyle(
-                            color: deepOrange,
-                          ),
-                        ),
+                      Text('Using'),
+                      SizedBox(
+                        height: 5,
                       ),
+                      Text('Mobile number'),
                     ],
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(
-                          horizontal: 30,
-                          vertical: 16,
-                        ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 17,
                       ),
-                      elevation: MaterialStateProperty.all(0),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          side: BorderSide(
-                            color: deepOrange,
-                          ),
-                        ),
-                      ),
-                      backgroundColor: MaterialStateProperty.all(deepOrange),
-                    ),
-                    child: Text('Sign in'),
+                      Text('Email id'),
+                    ],
                   ),
                 ],
               ),
-            ),
-          ],
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Phone number'),
+                          Row(
+                            children: [
+                              DropdownButton(
+                                  items: items.map((String items) {
+                                    return DropdownMenuItem(
+                                      value: items,
+                                      child: Text(items),
+                                    );
+                                  }).toList(),
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      dropdownValue = newValue!;
+                                    });
+                                  }),
+                              Container(
+                                height: 50,
+                                width: devWidth - 120,
+                                child: TextField(
+                                  onChanged: (value) {},
+                                  decoration: const InputDecoration(
+                                    hintText: 'Enter your phone number',
+                                    contentPadding: EdgeInsets.symmetric(
+                                      vertical: 10,
+                                      horizontal: 20,
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(32),
+                                      ),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Color.fromARGB(255, 93, 93, 93),
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(1),
+                                      ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.lightBlueAccent,
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(32),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Password'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: devWidth - 60,
+                                child: TextField(
+                                  onChanged: (value) {},
+                                  decoration: const InputDecoration(
+                                    hintText: 'Enter your password',
+                                    contentPadding: EdgeInsets.symmetric(
+                                      vertical: 10,
+                                      horizontal: 20,
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(32),
+                                      ),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Color.fromARGB(255, 93, 93, 93),
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(32),
+                                      ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.lightBlueAccent,
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(32),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 76,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('New user ?'),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(EdgeInsets.zero),
+                      elevation: MaterialStateProperty.all(0),
+                      backgroundColor: MaterialStateProperty.all(customWhite),
+                    ),
+                    child: Text(
+                      'Create an account',
+                      style: TextStyle(
+                        color: deepOrange,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 14,
+                    ),
+                  ),
+                  elevation: MaterialStateProperty.all(0),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      side: BorderSide(
+                        color: deepOrange,
+                      ),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(deepOrange),
+                ),
+                child: Text('working'),
+              ),
+            ],
+          ),
         ),
       ),
     );
