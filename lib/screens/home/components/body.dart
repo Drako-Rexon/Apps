d0a1767e55064d6c2079816bf9000b7368a379f1
Drako <abhishek491981@gmail.com>
2022-04-04 16:22:35 +0530

    one third done

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/header_with_search.dart';
 import 'package:plant_app/screens/home/components/title_with_more.dart';
 
@@ -12,9 +13,32 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           headerWithSearch(size: size),
-          titleWithMore(
-            title: 'Recomended',
-            press: () {},
+          titleWithMore(title: 'Recomended', press: () {}),
+          Container(
+            margin: EdgeInsets.only(
+              left: kDefaultPadding,
+              top: kDefaultPadding / 2,
+              bottom: kDefaultPadding * 0.25,
+            ),
+            width: size.width * 0.4,
+            child: Column(
+              children: [
+                Image.asset('assets/images/image_1.png'),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
